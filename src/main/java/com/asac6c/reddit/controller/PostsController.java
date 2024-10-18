65cb29846d02418ccbd57c3dc051035d39319a14
soleswan <eowls1358@gmail.com>
2024-10-18 23:35:24 +0900

    fix: postController postMapping 에서 getMapping 으로 변경

--- src/main/java/com/asac6c/reddit/controller/PostsController.java
+++ src/main/java/com/asac6c/reddit/controller/PostsController.java
@@ -5,8 +5,7 @@ import com.asac6c.reddit.dto.PostsResponseBody;
 import com.asac6c.reddit.service.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +15,9 @@ public class PostsController {
 
     private final PostsService postsService;
 
-    @PostMapping("/posts")
+    @GetMapping("/posts")
     @ResponseBody
-    public ResponseEntity<PostsResponseBody> posts(@RequestBody PostsRequestBody requestBody) {
+    public ResponseEntity<PostsResponseBody> posts(PostsRequestBody requestBody) {
 
         PostsResponseBody response = postsService.getPostsContents(requestBody);
 
