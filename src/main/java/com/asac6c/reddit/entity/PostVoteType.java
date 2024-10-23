41ad8b6c2159cc7e620e13173e3c62fb29e08503
Seok-Hun <hamer1233@naver.com>
2024-10-23 20:09:18 +0900

    Feat: Request로 받아오는 ENUM 데이터 검사

--- src/main/java/com/asac6c/reddit/entity/PostVoteType.java
+++ src/main/java/com/asac6c/reddit/entity/PostVoteType.java
@@ -1,6 +1,18 @@
 package com.asac6c.reddit.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum PostVoteType {
     LIKE,
-    DISLIKE
+    DISLIKE;
+
+    @JsonCreator
+    public static PostVoteType deserialize(String type){
+        for(PostVoteType postVoteType : PostVoteType.values()){
+            if(postVoteType.name().equals(type.toUpperCase())){
+                return postVoteType;
+            }
+        }
+        return null;
+    }
 }
