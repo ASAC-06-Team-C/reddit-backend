9a9651716a263ae12e73af3a40fefc5bebf3d0ac
namuuCY <wkdcjfdud13@gmail.com>
2024-10-25 17:40:11 +0900

    Init: Init for merge

---
+++ src/main/java/com/asac6c/reddit/dto/postDto/DraftUpsertRequestDto.java
@@ -0,0 +1,21 @@
+package com.asac6c.reddit.dto.postDto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@RequiredArgsConstructor
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class DraftUpsertRequestDto {
+
+  Integer userNo;
+  Integer postNo;
+  String postTitle;
+  String postContent;
+  boolean postDraft;
+}
