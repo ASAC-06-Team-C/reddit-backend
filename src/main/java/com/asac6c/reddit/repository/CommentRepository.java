bd127177bec6b551b9ef49c9609b5abd82ce7c3a
noname <jeonyo85@naver.com>
2024-10-27 02:15:06 +0900

    Feat : Comment post_no 필터 추가

--- src/main/java/com/asac6c/reddit/repository/CommentRepository.java
+++ src/main/java/com/asac6c/reddit/repository/CommentRepository.java
@@ -2,70 +2,78 @@ package com.asac6c.reddit.repository;
 
 import com.asac6c.reddit.dto.CommentRequestDTO;
 import com.asac6c.reddit.entity.CommentEntity;
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class CommentRepository {
+
   private final Map<Integer, CommentEntity> commentMap = new HashMap<>();
   private final Map<Integer, Set<Integer>> commentVoteMap = new HashMap<>();
-  
+
   private int comment_no = 1;
-  
-  public CommentEntity getComment(int comment_no) {
-    return commentMap.get(comment_no);
+
+  public List<CommentEntity> getComment(CommentRequestDTO.Read readRequest) {
+    return commentMap.values().stream()
+        .filter(comment -> comment.getPost_no() == readRequest.getPost_no())
+        .collect(Collectors.toList());
   }
-  
+
   public List<CommentEntity> getAllComment() {
     return new ArrayList<>(commentMap.values());
   }
-  
+
   public CommentEntity createComment(CommentRequestDTO.Create createRequest) {
     CommentEntity newComment = CommentEntity.from(createRequest, comment_no);
     commentMap.put(comment_no++, newComment);
     return newComment;
   }
-  
+
   public void updateComment(CommentRequestDTO.Update updateRequest) {
     CommentEntity comment = commentMap.get(updateRequest.getComment_no());
     CommentEntity updateComment = new CommentEntity(
-            comment.getComment_no(),
-            comment.getPost_no(),
-            updateRequest.getUser_no(),
-            updateRequest.getComment_content(),
-            comment.getComment_vote_count(),
-            comment.getComment_mother(),
-            comment.getComment_depth(),
-            LocalDateTime.now()
+        comment.getComment_no(),
+        comment.getPost_no(),
+        updateRequest.getUser_no(),
+        updateRequest.getComment_content(),
+        comment.getComment_vote_count(),
+        comment.getComment_mother(),
+        comment.getComment_depth(),
+        LocalDateTime.now()
     );
-    
+
     commentMap.replace(updateRequest.getComment_no(), updateComment);
   }
-  
+
   public void deleteComment(int comment_no) {
     commentMap.remove(comment_no);
   }
-  
+
   public void voteComment(CommentRequestDTO.Vote voteRequest) {
     CommentEntity comment = commentMap.get(voteRequest.getComment_no());
-    
+
     Set<Integer> vote = commentVoteMap.computeIfAbsent(voteRequest.getComment_no(),
-            k -> new HashSet<>());
-    
+        k -> new HashSet<>());
+
     if (voteRequest.isComment_vote_type()) {
       if (!vote.contains(voteRequest.getUser_no())) {
         vote.add(voteRequest.getUser_no());
         CommentEntity updateComment = new CommentEntity(
-                voteRequest.getComment_no(),
-                comment.getPost_no(),
-                voteRequest.getUser_no(),
-                comment.getComment_content(),
-                comment.getComment_vote_count() + 1,
-                comment.getComment_mother(),
-                comment.getComment_depth(),
-                LocalDateTime.now()
+            voteRequest.getComment_no(),
+            comment.getPost_no(),
+            voteRequest.getUser_no(),
+            comment.getComment_content(),
+            comment.getComment_vote_count() + 1,
+            comment.getComment_mother(),
+            comment.getComment_depth(),
+            LocalDateTime.now()
         );
         commentMap.replace(voteRequest.getComment_no(), updateComment);
       }
--- src/main/java/com/asac6c/reddit/service/CommentService.java
+++ src/main/java/com/asac6c/reddit/service/CommentService.java
@@ -4,37 +4,45 @@ import com.asac6c.reddit.dto.CommentRequestDTO;
 import com.asac6c.reddit.dto.CommentResponseDTO;
 import com.asac6c.reddit.entity.CommentEntity;
 import com.asac6c.reddit.repository.CommentRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class CommentService {
-  
+
+  @Autowired
   private final CommentRepository commentRepository;
-  
+
+  public List<CommentResponseDTO> getComment(CommentRequestDTO.Read readRequest) {
+    List<CommentEntity> comment = commentRepository.getComment(readRequest);
+    return comment.stream()
+        .map(commentEntity -> CommentResponseDTO.from(commentEntity, null, null))
+        .collect(Collectors.toList());
+  }
+
   public List<CommentResponseDTO> getAllComment(CommentRequestDTO.Read readRequest) {
     List<CommentEntity> comment = commentRepository.getAllComment();
     return comment.stream()
-            .map(commentEntity -> CommentResponseDTO.from(commentEntity, null, null))
-            .collect(Collectors.toList());
+        .map(commentEntity -> CommentResponseDTO.from(commentEntity, null, null))
+        .collect(Collectors.toList());
   }
-  
+
   public void createComment(CommentRequestDTO.Create createRequest) {
     CommentResponseDTO.from(commentRepository.createComment(createRequest), null, null);
   }
-  
+
   public void updateComment(CommentRequestDTO.Update updateRequest) {
     commentRepository.updateComment(updateRequest);
   }
-  
+
   public void deleteComment(CommentRequestDTO.Delete deleteRequest) {
     commentRepository.deleteComment(deleteRequest.getComment_no());
   }
-  
+
   public void voteComment(CommentRequestDTO.Vote voteRequest) {
     commentRepository.voteComment(voteRequest);
   }
