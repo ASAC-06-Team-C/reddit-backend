70fc411d4406027b9e6c355cb7183c903b856b2d
soleswan <eowls1358@gmail.com>
2024-10-29 16:02:24 +0900

    fix: 필요없는 어노테이션 삭제

--- src/main/java/com/asac6c/reddit/WebMvcConfig.java
+++ src/main/java/com/asac6c/reddit/WebMvcConfig.java
@@ -1,15 +1,11 @@
 package com.asac6c.reddit;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@EnableScheduling
-@EnableTransactionManagement
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
