74618890102419381a6c30988ea1a5df9850e1c9
noname <jeonyo85@naver.com>
2024-11-01 16:45:55 +0900

    Feat : voteComment 부족한 기능 구현

--- src/main/java/com/asac6c/reddit/entity/CommentVoteEntity.java
+++ src/main/java/com/asac6c/reddit/entity/CommentVoteEntity.java
@@ -7,18 +7,22 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class CommentVoteEntity {
-  
+
   private final int commentVoteNo;
   private final int userNo;
   private final int commentNo;
-  private final String commentVoteType;
-  
+  private String commentVoteType = null;
+
   public static CommentVoteEntity from(CommentRequestDTO.Vote voteRequest, int commentVoteNo) {
     return new CommentVoteEntity(
-            commentVoteNo,
-            voteRequest.getUserNo(),
-            voteRequest.getCommentNo(),
-            voteRequest.getCommentVoteType()
+        commentVoteNo,
+        voteRequest.getUserNo(),
+        voteRequest.getCommentNo()
+//        voteRequest.getCommentVoteType()
     );
+  }
+
+  public void changeVoteType(String commentVoteType) {
+    this.commentVoteType = commentVoteType;
   }
 }
--- src/main/java/com/asac6c/reddit/repository/CommentRepository.java
+++ src/main/java/com/asac6c/reddit/repository/CommentRepository.java
@@ -5,75 +5,85 @@ import com.asac6c.reddit.dto.CommentRequestDTO.Update;
 import com.asac6c.reddit.dto.CommentRequestDTO.Vote;
 import com.asac6c.reddit.entity.CommentEntity;
 import com.asac6c.reddit.entity.CommentVoteEntity;
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class CommentRepository {
-  
+
   private final Map<Integer, CommentEntity> commentMap = new HashMap<>();
+  // 게시글에서 가져오는거(댓글번호와 그 댓글번호에 해당하는 좋아요 집합)
   private final Map<Integer, CommentVoteEntity> commentVoteMap = new HashMap<>();
-  
+  //
+//  private final Map<Integer, Set<CommentVoteEntity>> commentVoteMap = new HashMap<>();
+
   private int commentNo = 1;
   private int commentVoteNo = 1;
-  
+
   public List<CommentEntity> getComment(int postNo) {
     return commentMap.values().stream()
-            .filter(comment -> comment.getPostNo() == postNo)
-            .toList();
+        .filter(comment -> comment.getPostNo() == postNo)
+        .toList();
   }
-  
+
   public List<CommentEntity> getAllComment() {
     return new ArrayList<>(commentMap.values());
   }
-  
+
   public CommentEntity createComment(Create createRequest) {
     CommentEntity newComment = CommentEntity.from(createRequest, commentNo);
-    
+
     commentMap.put(commentNo++, newComment);
     return newComment;
   }
-  
+
   public void updateComment(Update updateRequest) {
     CommentEntity comment = commentMap.get(updateRequest.getCommentNo());
-    
+
     comment.setCommentContent(updateRequest.getCommentContent());
   }
-  
+
   public void deleteComment(int comment_no) {
     commentMap.remove(comment_no);
   }
-  
+
   public void voteComment(Vote voteRequest) {
+    // 유저 번호에 대한 vote 내역 가져오기
+    CommentVoteEntity voteValid = commentVoteMap.values().stream()
+        .filter(vote -> vote.getUserNo() == voteRequest.getUserNo()
+            && vote.getCommentNo() == voteRequest.getCommentNo())
+        .findFirst()
+        .orElse(null);
+
+    // 투표를 하지 않았다면,
+    if (voteValid == null) {
+      CommentVoteEntity newVote = CommentVoteEntity.from(voteRequest, commentVoteNo);
+      newVote.changeVoteType(voteRequest.getCommentVoteType());
+      commentVoteMap.put(commentVoteNo++, newVote);
+    }
+    // 투표를 했다면
+    else {
+      CommentVoteEntity updateVote = CommentVoteEntity.from(voteRequest,
+          voteValid.getCommentVoteNo());
+      updateVote.changeVoteType(voteRequest.getCommentVoteType());
+      commentVoteMap.put(voteValid.getCommentVoteNo(), updateVote);
+    }
+
+    updateVoteCount(voteRequest);
+  }
+
+  private void updateVoteCount(Vote voteRequest) {
     CommentEntity comment = commentMap.get(voteRequest.getCommentNo());
-    int voteCount = comment.getCommentVoteCount();
-    
-    // 1. 댓글 no와 유저 no으로 commentVoteMap에서 투표를 했는지 확인.
-    // 2. 투표를 하지 않았다면, 투표 타입에 맞게 up 또는 down
-    // 3. 투표를 했다면, up, none, down에 맞춰서 생각.
-    // 4. 그 전에 했던 좋아요 타입에 따라 계산?
-    
-    commentVoteMap.put(commentVoteNo, CommentVoteEntity.from(voteRequest, commentVoteNo++));
 
-//    System.out.println(voteRequest.getCommentVoteType());
-//    if (voteRequest.getCommentVoteType().equals("up")) {
-//      voteCount = comment.getCommentVoteCount() + 1;
-//    } else if (voteRequest.getCommentVoteType().equals("down")) {
-//      voteCount = comment.getCommentVoteCount() - 1;
-//    } else if (voteRequest.getCommentVoteType().equals("uptodown")) {
-//      voteCount = comment.getCommentVoteCount() - 2;
-//    } else if (voteRequest.getCommentVoteType().equals("downtoup")) {
-//      voteCount = comment.getCommentVoteCount() + 2;
-//    }
+    int voteCount = commentVoteMap.values().stream()
+        .filter(vote -> vote.getCommentNo() == voteRequest.getCommentNo())
+        .mapToInt(vote -> "UP".equals(vote.getCommentVoteType()) ? 1
+            : "DOWN".equals(vote.getCommentVoteType()) ? -1 : 0)
+        .sum();
 
-//    int voteCount = voteRequest.isCommentVoteType()
-//        ? comment.getCommentVoteCount() + 1
-//        : comment.getCommentVoteCount() - 1;
-    
     comment.setCommentVoteCount(voteCount);
   }
 }
