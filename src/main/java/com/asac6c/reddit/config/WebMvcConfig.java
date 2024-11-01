7f56347e9c560f4d6e784e5cb0e29baace846784
Seok-Hun <hamer1233@naver.com>
2024-11-02 07:04:49 +0900

    Feat: CORS 이슈 해결을 위한 config 파일 추가

---
+++ src/main/java/com/asac6c/reddit/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.asac6c.reddit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+    }
+}
