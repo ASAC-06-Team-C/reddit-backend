4b8fb06f2310476d47521fe814af729e9907c8a1
namuuCY <wkdcjfdud13@gmail.com>
2024-11-06 10:38:58 +0900

    Init: EC2 서버 구동을 위한 branch

--- src/main/java/com/asac6c/reddit/WebMvcConfig.java
+++ src/main/java/com/asac6c/reddit/WebMvcConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("http://namucy-s3-v1.s3-website.ap-northeast-2.amazonaws.com")
+        .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+  }
 }
